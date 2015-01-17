fdf34c8738874e840debf817bcb3f298f1d305fc
Jim Fingal <jim.fingal@gmail.com>
2015-01-16 21:04:35 -0800

    store hashes of 'real' sentences so we don't repeat them

---
+++ hash_existing.py
@@ -0,0 +1,32 @@
+from gevent import monkey
+monkey.patch_all()
+
+import logging
+
+import redis 
+
+from botutils import config, ai
+from ebookofblackearth import aesirbot, storage
+
+log_fmt = "%(levelname)-6s %(processName)s %(filename)-12s:%(lineno)-4d at %(asctime)s: %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+# TODO -- centralize
+APP_NAME = "AESIRBOT"
+REDIS_COLLECTION = 'posted'
+
+if __name__ == "__main__":
+
+    ai.boostrap_nltk_data()
+
+    storage.connect_mongo(APP_NAME)
+    redis_client = redis.from_url(config.get_redis_url(APP_NAME))
+
+    for corpus_sentence in storage.CorpusText.objects():
+        try:
+            text_hash = aesirbot.get_text_hash(corpus_sentence.text)
+            print corpus_sentence.text
+            print text_hash
+            redis_client.sadd(REDIS_COLLECTION, text_hash)
+        except UnicodeEncodeError as e:
+            print e
