6cc34d0269e7717e56bfb770006eed59b288e3c7
Jim Fingal <jim.fingal@gmail.com>
2015-01-16 17:10:20 -0800

    clean tweets

--- ebookofblackearth/aesirbot.py
+++ ebookofblackearth/aesirbot.py
@@ -69,7 +69,8 @@ def clean_tweet(s):
     s = re.sub(r'\W+$','',s)
     s = s.replace('(', '')
     s = s.replace(')', '')
-
+    s = s.replace('`', '')
+    s = s.replace('- ', '—')
     for word in AESIR_WORDS:
         s = s.replace(word, word.capitalize())
     s = s.replace(" i ", " I ")
